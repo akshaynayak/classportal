95b3b5b8b5ca82f72d47d572394cb41f18727c36
akshaynayak <akshaynayak2012@gmail.com>
2016-02-27 00:16:58 -0500

    removed material from view

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -112,6 +112,6 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      params.require(:course).permit(:course_number, :title, :description, :start_date, :end_date, :is_active, :notifications, :deadlines, :material)
+      params.require(:course).permit(:course_number, :title, :description, :start_date, :end_date, :is_active, :notifications, :deadlines)
     end
 end
