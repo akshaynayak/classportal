9e11a0ea326b0e6d529a3caec15fa4d7799d32c2
akshaynayak <akshaynayak2012@gmail.com>
2016-02-26 23:46:21 -0500

    present?check

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,13 +7,33 @@ class UserTest < ActiveSupport::TestCase
   # end
 
   def setup
-    @User=User.new(name:"Demo user", email: "demo@ncsu.edu", password: "123456")
+    @User = User.new(name: "Demo user", email: "demo@ncsu.edu", password: "password123")
   end
 
-  test "email should be present" do
-
+  test "email should not be blank" do
+    @user.email = "     "
+    assert_not @user.valid?
   end
 
+  test "should be valid" do
+    assert @User.valid?, "User details not saved"
+  end
+
+  test "name should not be blank" do
+    @user.name = "   "
+    assert_not @user.valid?
+  end
+
+  test "email address should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should not be blank (nonblank)" do
+    @user.password =  " "
+    assert_not @user.valid?
+  end
 
 end
 
